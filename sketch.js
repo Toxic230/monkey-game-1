c7d7d16ee5467826218d9d7763fa53bf5e9b7127
Toxic230 <72295122+Toxic230@users.noreply.github.com>
2020-10-20 18:21:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,93 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstacleGroup
+var suvivalTime=0
+var score=0
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+function setup() {
+  
+monkey= createSprite(80,315,20,20) ;
+  monkey.addAnimation ("moving",monkey_running);
+  monkey.scale=0.1
+  
+  ground=createSprite(400,350,900,10)
+  ground.velocityX=-4
+  ground.x=ground.width/2;
+  console.log(ground.x)
+
+}
+
+
+function draw() {
+background(255);
+  
+monkey.collide(ground)
+
+  stroke("white")
+  textSize(20);
+  fill("white")
+  text("score:"+ score,500,50);
+  
+  stroke("black")
+  textSize(20);
+  fill("black");
+  survivalTime=Math.ceil(frameCount/frameRate())
+  text("survival Time:"+ survivalTime,100,50);
+  
+  
+  
+  if(ground.x<0){
+    ground.x=ground.width/2
+  }
+if(keyDown("space")){
+  monkey.velocityY=-12
+}
+monkey.velocityY=monkey.velocityY+0.8
+  
+  monkey.collide(ground)
+  
+
+drawSprites();
+
+
+spawnBanana()
+  
+ 
+spawnObstacles();
+  
+}
+
+function spawnBanana(){
+  if(frameCount % 80 === 0){
+   banana=createSprite(200,200);
+  banana.addImage(bananaImage)
+  banana.scale=0.1
+  banana.velocityX=monkey.velocityX   
+  drawSprites();
+    banana.velocityX=-4
+    banana.y=Math.round(random(120,200))
+  }
+} 
+
+function spawnObstacles(){
+  
+    if(frameCount % 300===0){
+ obstacle=createSprite(200,310,80,80)
+  obstacle.addImage(obstaceImage)
+  obstacle.scale=0.2
+ obstacle.velocityX=-4
+obstacle.lifetime=200
+    }
+  
+}
